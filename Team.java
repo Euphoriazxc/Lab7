db5587d8d1c2a35dbe6de410c492348c9b08e25d
Euphoriazxc <79075123+Euphoriazxc@users.noreply.github.com>
2021-12-23 12:24:46 +0900

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class Team implements Serializable {
+String name;
+String city;
+int place;
+int victories;
+@Override
+public String toString() {
+return "Team{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", place=" + place +
+                ", victories=" + victories +
+                '}';
+}
+}
